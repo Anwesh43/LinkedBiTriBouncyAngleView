7cfcf667db72e26e3fe4fb4f137512664b4d4efa
Anwesh43 <anweshthecool0@gmail.com>
2019-12-29 14:25:05 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbitribouncyangleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbitribouncyangleview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedbitribouncyangleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.bitribouncyangleview.BiTriBouncyAngleView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BiTriBouncyAngleView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
