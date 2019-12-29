75318cde0fce64389900aa188fbc73889cd5270b
Anwesh43 <anweshthecool0@gmail.com>
2019-12-29 14:24:29 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbitribouncyangleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbitribouncyangleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbitribouncyangleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bitribouncyangleview.BiTriBouncyAngleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiTriBouncyAngleView.create(this)
     }
 }
