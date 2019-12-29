67233808d1e4362051d4cfc942332d18f5649179
Anwesh43 <anweshthecool0@gmail.com>
2019-12-29 13:29:22 +0530

    adding constants and libraries

--- bitribouncyangleview/src/main/java/com/anwesh/uiprojects/bitribouncyangleview/BiTriBouncyAngleView.kt
+++ bitribouncyangleview/src/main/java/com/anwesh/uiprojects/bitribouncyangleview/BiTriBouncyAngleView.kt
@@ -4,3 +4,21 @@ package com.anwesh.uiprojects.bitribouncyangleview
  * Created by anweshmishra on 29/12/19.
  */
 
+import android.content.Context
+import android.app.Activity
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.02f / lines
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rotDeg : Float = 45f
+val delay : Long = 30
+
